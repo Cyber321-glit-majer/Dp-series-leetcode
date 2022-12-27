bcec30fc8e62f509092a4206b4d3cca4d9fbc825
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2022-12-27 19:04:49 +0530

    Create 1137. N-th Tribonacci Number.cpp

---
+++ 1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+    
+if(n==0)
+return n;
+if(n==1 || n==2)
+return 1;
+//return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+  int f[n+2];
+  f[0]=0;
+  f[1]=1;
+  f[2]=1;
+  for(int i=3;i<=n;i++)
+  {
+      f[i]=f[i-1]+f[i-2]+f[i-3];
+  }
+  return f[n];
+
+   
+
+
+    }
+};
