46c1459e2fa38be3a5b5b6b6a1005fdd1145c199
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2022-12-27 19:30:50 +0530

    Update 1137. N-th Tribonacci Number.cpp

--- 1137. N-th Tribonacci Number.cpp
+++ 1137. N-th Tribonacci Number.cpp
@@ -22,3 +22,38 @@ return 1;
 
     }
 };
+
+
+// space optomization 
+
+class Solution {
+public:
+    int tribonacci(int n) {
+    
+if(n==0)
+return n;
+if(n==1 || n==2)
+return 1;
+//return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+  
+  int x=0;
+  int y=1;
+  int z=1;
+  int a=0;
+  for(int i=3;i<=n;i++)
+  {
+      //f[i]=f[i-1]+f[i-2]+f[i-3];
+      
+     a=x+y+z;
+x=y;
+y=z;
+z=a;
+
+      
+  }
+  return a;
+
+ 
+
+    }
+};
