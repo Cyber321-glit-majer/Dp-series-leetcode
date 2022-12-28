4c661ffc93c884d31b3c988f8bab3e6d1cb4bb8f
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2022-12-28 17:41:14 +0530

    Update Maximum sum of non-adjacent elements.cpp

--- Maximum sum of non-adjacent elements.cpp
+++ Maximum sum of non-adjacent elements.cpp
@@ -1,3 +1,7 @@
+//Tabulation method
+TC- o(N)
+     SC: o(N)
+
 #include <bits/stdc++.h> 
 int solve(vector<int>&nums )
 {
@@ -11,6 +15,36 @@ int solve(vector<int>&nums )
         dp[i]=max(inc,exc);
     }
     return dp[n-1];
+}
+int maximumNonAdjacentSum(vector<int> &nums){
+   // int n=nums.size();
+   // vector<int>dp(n,-1);
+    return solve(nums);
+  
+    
+}
+
+// space optimization 
+TC-o(N)
+     SC-o(1)
+
+#include <bits/stdc++.h> 
+int solve(vector<int>&nums )
+{
+     int n=nums.size();
+    //vector<int>dp(n,0);
+    //dp[0]=nums[0];
+    int prev2=0;
+    int prev1=nums[0];
+    for(int i=1;i<n;i++)
+    {
+        int inc=prev2+nums[i];
+        int exc=prev1+0;
+       int ans=max(inc,exc);
+        prev2=prev1;
+        prev1=ans;
+    }
+    return prev1;
 }
 int maximumNonAdjacentSum(vector<int> &nums){
    // int n=nums.size();
